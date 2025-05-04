c5a3cc2d3a6b716d564b3c33d377de5f5be0c2ee
Troy Pilewski <troy.pilewski@gmail.com>
2025-05-04 16:36:48 -0700

    Updated Main.java

--- src/main/java/com/avaruusstudios/vmdb/Main.java
+++ src/main/java/com/avaruusstudios/vmdb/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/avaruusstudios/vmdb/views/main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        stage.setTitle("Vanpool Management Database");
         stage.setScene(scene);
         stage.show();
     }
