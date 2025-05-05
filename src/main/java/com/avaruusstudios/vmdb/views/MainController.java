d9f245703167cb7d6d6581e956262d508bcba0ec
Troy Pilewski <troy.pilewski@gmail.com>
2025-05-04 17:05:29 -0700

    Updated main.fxml and MainController.java

--- src/main/java/com/avaruusstudios/vmdb/views/MainController.java
+++ src/main/java/com/avaruusstudios/vmdb/views/MainController.java
@@ -1,14 +1,4 @@
 package com.avaruusstudios.vmdb.views;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class MainController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
