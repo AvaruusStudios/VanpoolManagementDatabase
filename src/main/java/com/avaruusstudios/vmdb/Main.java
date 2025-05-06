faa794226961b2d777fec4a69106e0fcbb8d5e03
Troy Pilewski <troy.pilewski@gmail.com>
2025-05-05 21:19:14 -0700

    Updated Main.java and MainController.java

--- src/main/java/com/avaruusstudios/vmdb/Main.java
+++ src/main/java/com/avaruusstudios/vmdb/Main.java
@@ -1,5 +1,6 @@
 package com.avaruusstudios.vmdb;
 
+import com.avaruusstudios.vmdb.db.Database;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,7 +11,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/avaruusstudios/vmdb/views/main.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/avaruusstudios/vmdb/views/main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
         stage.setTitle("Vanpool Management Database");
         stage.setScene(scene);
@@ -18,6 +20,8 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        // Initialize the database (create if it doesn't exist)
+        Database.createDatabase();
         launch();
     }
 }
--- src/main/java/com/avaruusstudios/vmdb/views/MainController.java
+++ src/main/java/com/avaruusstudios/vmdb/views/MainController.java
@@ -1,14 +1,21 @@
 package com.avaruusstudios.vmdb.views;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TreeCell;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.util.Callback;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainController {
     @FXML
     private TreeView<String> navigationTreeView;
+    @FXML
+    private TableView<Map<String, Object>> mainTableView;
 
     public void initialize() {
         /** Creates a root item in the TreeView Navigation */
@@ -45,18 +52,61 @@ public class MainController {
         navigationTreeView.setCellFactory(new Callback<TreeView<String>, TreeCell<String>>() {
             @Override
             public TreeCell<String> call(TreeView<String> param) {
-                NavigationTreeCell cell = new NavigationTreeCell();
+                NavigationTreeCell cell = new NavigationTreeCell(root, MainController.this);
                 cell.setRootItem(root);
                 return cell;
             }
         });
     }
 
+    /** Loads data into the TableView based on the provided SQL query */
+    private void loadTableData(String sqlQuery) {
+        ObservableList<Map<String, Object>> data = FXCollections.observableArrayList();
+        mainTableView.getColumns().clear(); // Clear any existing columns
+
+        String url = "jdbc:sqlite:./vanpool.db";
+        try (Connection connection = DriverManager.getConnection(url);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sqlQuery)) {
+
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            List<String> columnNames = new ArrayList<>();
+            for (int i = 1; i <= columnCount; i++) {
+                String columnName = metaData.getColumnName(i);
+                columnNames.add(columnName);
+                TableColumn<Map<String, Object>, Object> column = new TableColumn<>(columnName);
+                final String finalColumnName = columnName; // Need a final variable for the lambda
+
+                column.setCellValueFactory(cellData ->
+                        new javafx.beans.property.ReadOnlyObjectWrapper<>(cellData.getValue().get(finalColumnName)));
+
+                mainTableView.getColumns().add(column);
+            }
+
+            while (resultSet.next()) {
+                Map<String, Object> row = new HashMap<>();
+                for (String columnName : columnNames) {
+                    row.put(columnName, resultSet.getObject(columnName));
+                }
+                data.add(row);
+            }
+
+            mainTableView.setItems(data);
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception properly
+        }
+    }
+
     /** Custom TreeCell Factory */
     private static class NavigationTreeCell extends TreeCell<String> {
         private TreeItem<String> rootItem;
+        private final MainController controller;
 
-        public NavigationTreeCell() {
+        public NavigationTreeCell(TreeItem<String> rootItem, MainController controller) {
+            this.rootItem = rootItem;
+            this.controller = controller;
             setOnMouseClicked(event -> {
                 if (!isEmpty() && event.getClickCount() == 1) {
                     String item = getItem();
@@ -64,6 +114,7 @@ public class MainController {
                         switch (item) {
                             case "Participants":
                                 System.out.println("TreeCell Clicked: Participants");
+                                this.controller.loadTableData("SELECT * FROM Participants");
                                 break;
                             case "Add Participant":
                                 System.out.println("TreeCell Clicked: Add Participant");
