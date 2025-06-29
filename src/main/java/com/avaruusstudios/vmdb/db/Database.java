989d3b2d0e6282f5251b6c61fdb6daef564327a5
Troy Pilewski <troy.pilewski@gmail.com>
2025-06-29 12:44:48 -0700

    Fixed spacing in the import statements

--- src/main/java/com/avaruusstudios/vmdb/db/Database.java
+++ src/main/java/com/avaruusstudios/vmdb/db/Database.java
@@ -6,7 +6,6 @@ import com.avaruusstudios.vmdb.model.EventType;
 import com.avaruusstudios.vmdb.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
